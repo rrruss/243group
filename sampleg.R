46b19511f3c087313c419bdc26cdb7d33251309d
U-rusty\rrruss <rrruss@rusty.(none)>
2013-12-09 21:21:31 -0500

    fixed the vector huz in sampleg

--- sampleg.R
+++ sampleg.R
@@ -25,11 +25,11 @@ sampleSX <- function(g,n) {
   #hk is a vector of h(x)=log(f(x)) the k abscissae x_i
   #hpx is a vector of h'(x) of the k abscissae x_i
   #zk is the intersection points of the tangents
-  xk <- g[,3]
+  xk <- g[,'intersect']
   k <- length(xk)
-  hk <- g[,5]
-  hpk <- (g[,4])*xk + g[,5]
-  zk <- c(g[,1],g[2,k]) #note that there are (k+1) z values
+  hk <- g[,'b']
+  hpx <- g[,'slope']
+  zk <- c(g[,'start'],g[k,'end']) #note that there are (k+1) z values
   
   #calculate the areas of the k chunks (after exponentiating):
 #   areas <- rep(NA,k)
@@ -37,8 +37,17 @@ sampleSX <- function(g,n) {
 #   for (i in 1:k) {
 #     areas[i] <- (exp(zk[i+1])-exp(zk[i]))/hpx[i]
 #   }
+  
+  #to calculate the value of the upper envelope at the z points, interpolate using the tangent lines:
+  xdummy <- c(0,xk)
+  zkMinusxk <- zk - xdummy
+  huz <- hpx*(zkMinusxk[-1]) +hk
+  #the first value (at zk[1]) is calculated manually:
+  huz0 <- huz[1] - hpx[1]*(zk[2]-zk[1])
+  huz <- c(huz0,huz)
+  
   #here it is, a vectorized version of computing the areas:
-  t <- exp(c(zk,1)) - exp(c(1,zk))
+  t <- exp(c(huz,1)) - exp(c(1,huz))
   areas <- t[-c(1,k+2)]/hpx
   
   #normalizing factor of the exponentiated upper hull:
@@ -56,10 +65,10 @@ sampleSX <- function(g,n) {
   #try a vectorized version:
   u <- runif(n)
   whichChunk <- rep(1,n)
-  for (k in 1:n) {
-    while (u[k] > scum[whichChunk[k]+1])
-    whichChunk[k] <- whichChunk[k] + 1
+  for (j in 1:n) {
+    while (u[j] > scum[whichChunk[j]+1])
+    whichChunk[j] <- whichChunk[j] + 1
   }
-  sample <- zk[whichChunk] + log(1 + (hpx[whichChunk+1])*normFactor*(u-scum[whichChunk]) / (exp(zk[whichChunk])) )/hpx[whichChunk+1]
+  sample <- zk[whichChunk] + log(1 + (hpx[whichChunk])*normFactor*(u-scum[whichChunk]) / (exp(huz[whichChunk])) )/hpx[whichChunk]
   return(sample)
 }
